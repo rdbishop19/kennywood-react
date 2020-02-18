b1d187f9dbadbe276eaa81b5428aa89a5b505be6
Ryan Bishop <rdbishop06@gmail.com>
2020-02-18 15:22:17 -0600

    feat: login and isAuthenticated functions

--- src/components/helpers/simpleAuth.js
+++ src/components/helpers/simpleAuth.js
@@ -1,4 +1,27 @@
 const BASE_URL = 'http://localhost:8000'
+const STORAGE_TOKEN = "kennywood_token"
+
+const isAuthenticated = () => {
+    return sessionStorage.getItem(STORAGE_TOKEN) !== null
+}
+
+const login = credentials => {
+    return fetch(`${BASE_URL}/login/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log('login', res)
+        if ("valid" in res && res.valid && 'token' in res) {
+            sessionStorage.setItem(STORAGE_TOKEN, res.token)
+        }
+    })
+}
 
 const register = (userInfo) => {
     return fetch(`${BASE_URL}/register/`, {
@@ -12,9 +35,9 @@ const register = (userInfo) => {
     .then(res => res.json())
     .then(res => {
         if ("token" in res) {
-            sessionStorage.setItem('kennywood_token', res.token)
+            sessionStorage.setItem(STORAGE_TOKEN, res.token)
         }
     })
 }
 
-export { register }
+export { isAuthenticated, login, register }
